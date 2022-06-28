1e0ad017139c4b1b4675acadbdd0ebe14f2ec7b1
Joel Nicolaj Pedersen <nicolajlpedersen@gmail.com>
2022-06-28 15:25:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name = input("What's your name, stranger?\n")
+
+print("Greetings, " + name)
+
+city = input(name + ", what's the city you grew up in?\n")
+
+pet = input("And what was the name of your favorit pet?\n")
+
+print('I think I got the perfect name for your band, ' + name + ', how about "' + city + " " + pet + '"')
+
+print("The Worlds Most Popular Band In The World: " + '"' + city + " " + pet + '"')
